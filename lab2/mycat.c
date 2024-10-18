9c1cc9e92bc6e3fe8149ec0974a8126de4d813ed
Иван Покровский <ivan.d.pokrovsky@gmail.com>
2024-10-18 15:03:13 +0300

    labs

--- lab2/mycat.c
+++ lab2/mycat.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h> 
 
 void mycat(int argc, char *argv[], int showEnds, int numberNonblank, int numberAll) {
     int lineNumber = 1;
     FILE *file;
 
-    for (int i = 1; i < argc; i++) {
-        if (argv[i][0] != '-') { // это файл, а не флаг
-            file = fopen(argv[i], "r");
-            if (file == NULL) {
-                perror("mycat");
-                exit(1);
-            }
-
-            char line[1024];
-            while (fgets(line, sizeof(line), file)) {
-                if (numberAll || (numberNonblank && strlen(line) > 1)) {
-                    printf("%6d\t", lineNumber++);
-                }
-                printf("%s", line);
-                if (showEnds) printf("$");
-            }
-
-            fclose(file);
+    for (int i = optind; i < argc; i++) { 
+        file = fopen(argv[i], "r");
+        if (file == NULL) {
+            perror("mycat");
+            exit(1);
         }
+
+        char line[1024];
+        while (fgets(line, sizeof(line), file)) {
+            if (numberAll || (numberNonblank && strlen(line) > 1)) {
+                printf("%6d\t", lineNumber++);
+            }
+            printf("%s", line);
+            if (showEnds) printf("$");
+        }
+
+        fclose(file);
     }
 }
 
@@ -35,18 +34,27 @@ int main(int argc, char *argv[]) {
     }
 
     int showEnds = 0, numberNonblank = 0, numberAll = 0;
+    int opt;
 
-    // Сначала обрабатываем все флаги
-    for (int i = 1; i < argc; i++) {
-        if (argv[i][0] == '-') {
-            char *option = argv[i] + 1;
-            if (strchr(option, 'E')) showEnds = 1;
-            if (strchr(option, 'b')) numberNonblank = 1;
-            if (strchr(option, 'n')) numberAll = 1;
+    
+    while ((opt = getopt(argc, argv, "Ebn")) != -1) {
+        switch (opt) {
+            case 'E':
+                showEnds = 1;
+                break;
+            case 'b':
+                numberNonblank = 1;
+                break;
+            case 'n':
+                numberAll = 1;
+                break;
+            default: /* '?' */
+                fprintf(stderr, "Usage: %s [-E] [-b] [-n] [FILE]...\n", argv[0]);
+                exit(EXIT_FAILURE);
         }
     }
 
-    // Потом обрабатываем файлы
+    // Вызов функции для обработки файлов
     mycat(argc, argv, showEnds, numberNonblank, numberAll);
     
     return 0;
